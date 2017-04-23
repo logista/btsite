b5326b28db8a9f18659027f007a7841fed1221ca
Barbara Tozier <logista@gmail.com>
2017-04-22 22:47:31 -0400

    new version to do subdirectories

--- _plugins/imageener.rb
+++ _plugins/imageener.rb
@@ -6,30 +6,32 @@ module Jekyll
 
       src = site.config["picserizer"]["source"]
       source_dir = File.join(site.source,src)
+      Dir.chdir(source_dir)
+      all_subdirectories = Dir.glob("*").find_all {|i| File.directory?(i)}
       sizes = site.config["picserizer"]["sizes"]
       target_dir = File.join(site.source,site.config["picserizer"]["target"])
 
-      our_pictures = Dir.glob("#{source_dir}/*.jpg")
+      all_subdirectories.each do |subdir|
+        source_pictures = Dir.glob("#{source_dir}/#{subdir}/*.jpg")
 
-      sizes.each do |size|
-        full_path = "#{target_dir}/#{size}"
-        if Dir.exist?(full_path)
-        #   puts "I found #{full_path}!"
-        else
-          Dir.mkdir(full_path)
-        end
-      end
-
-      our_pictures.each do |pic_name|
         sizes.each do |size|
-          new_name = "#{target_dir}/#{size.to_s}/" + File.basename(pic_name)
-          if File.exist?(new_name)
-            # puts "There's already a #{new_name}!"
+          full_target_path = "#{target_dir}/#{subdir}/#{size.to_s}"
+          if Dir.exist?(full_target_path)
+            # puts "I found #{full_target_path}!"
           else
-            loaded_pic = MiniMagick::Image.open(pic_name)
-            loaded_pic.resize size.to_s
-            loaded_pic.format "jpg"
-            loaded_pic.write(new_name)
+            Dir.mkdir(full_target_path)
+          end
+
+          source_pictures.each do |pic_name|
+            new_name = full_target_path + "/" + File.basename(pic_name)
+            if File.exist?(new_name)
+              # puts "There's already a #{new_name}!"
+            else
+              loaded_pic = MiniMagick::Image.open(pic_name)
+              loaded_pic.resize size.to_s
+              loaded_pic.format "jpg"
+              loaded_pic.write(new_name)
+            end
           end
         end
       end
